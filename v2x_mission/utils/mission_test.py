595392441cea911dff84bd9f2df6db3a36b1f219
Kim Kana <kimgana96@gmail.com>
2022-10-18 09:09:08 +0900

    [v2x_mission]utils: mission_test.py created

---
+++ v2x_mission/utils/mission_test.py
@@ -0,0 +1,45 @@
+import rospy
+import time
+from std_msgs.msg import Int16MultiArray
+
+class MissionTest:
+    def __init__(self):
+        rospy.init_node('MissionTest')
+        self.stage1_state_pub = rospy.Publisher('/stage1_state', Int16MultiArray, queue_size=10 )
+        rospy.Subscriber('/arrive_info', Int16MultiArray, self.arrive_info_cb)
+        self.arrive_info = [0,0]
+        self.arr = Int16MultiArray()
+        self.stage1_state = []
+    
+    def arrive_info_cb(self, msg):
+        self.arrive_info = msg.data
+
+    def updateState(self):
+        rate = rospy.Rate(10)
+        start_time = time.time()
+        self.stage1_state = [4, 0, 0, 0]
+
+        while not rospy.is_shutdown():
+            if(time.time()-start_time == 10):
+                self.stage1_state = [4, 1, 0, 0]
+            elif(self.stage1_state[1] and not self.stage1_state[2]and not self.stage1_state[3]):
+                if(self.arrive_info[0]):
+                    self.stage1_state = [4, 1, 1, 0]
+            elif(self.stage1_state[1] and self.stage1_state[2]and not self.stage1_state[3]):
+                if(self.arrive_info[1]):
+                    self.stage1_state = [4, 1, 1, 1]
+            elif(self.stage1_state[1] and self.stage1_state[2]and self.stage1_state[3]):
+                self.stage1_state = [4, 0, 0, 0]
+                self.arrive_info = [0, 0]
+                break 
+            self.arr.data = self.stage1_state
+            self.stage1_state_pub.publish(self.arr)
+            rate.sleep()
+        rospy.spin()
+
+if __name__ == '__main__':
+    t = MissionTest()
+    try:
+        t.updateState()
+    except rospy.ROSInternalException:
+        pass
