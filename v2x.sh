a4c17e54a1b7160952cbe3e032a597a2fdacc109
inha <Nuvo>
2022-10-23 09:14:40 +0900

    [v2x]generate quick starter

---
+++ v2x.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+source /opt/ros/melodic/setup.sh
+source /home/inha/catkin_ws/devel/setup.sh
+export ROS_MASTER_URI=http://192.168.101.1:11311
+export ROS_HOSTNAME=192.168.101.1
+
+roslaunch /home/inha/ros_ws/src/KIAPI_INHA/v2x_parser/launch/v2x_parser.launch
+
